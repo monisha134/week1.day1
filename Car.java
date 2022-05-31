ed2de22dc82476007f09ce1de8ec230e02ee5a26
monisha134 <106609951+monisha134@users.noreply.github.com>
2022-05-31 08:20:58 -0700

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,18 @@
+package weeek1.day1;
+
+public class Car {
+	
+	public void drivecar() {
+		
+	System.out.println("Drive car");
+	
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("inside main method");
+		Car myCar= new Car();
+        myCar.drivecar();
+		
+	}
+
+}
